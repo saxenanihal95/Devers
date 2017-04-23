f6956a22ade40343f1f2af82ce6238a09acda31c
nihal <saxenanihal95@gmail.com>
2017-04-23 19:30:59 +0530

    changes

--- classes/Mail.php
+++ classes/Mail.php
@@ -11,8 +11,8 @@ class Mail
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'nihalsxn@gmail.com';                 // SMTP username
-    $mail->Password = 'nhl$1xen1';                           // SMTP password
+    $mail->Username = 'deversorg@gmail.com';                 // SMTP username
+    $mail->Password = 'asdf1234$';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = '587';                                    // TCP port to connect to
 
